e3753abde5e281cc5fad1fc18c05ceee37d488e7
DandinPower <t108590045@ntut.org.tw>
2021-09-06 13:25:00 +0800

    新增單層三顆神經元單位

--- simnn.py
+++ simnn.py
@@ -4,15 +4,34 @@ import matplotlib.pyplot as plt
 import random
 import math
 import time
-
 def randomfloat(a,b,c):
-    return random.randint(a, b)/10
+    return random.randint(a, b)/c
+
+def abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+def posorneg(x,target):
+    pos = 0
+    neg = 0
+    for data in x:
+        if data > 0:
+            pos += 1
+        elif data <0:
+            neg += 1
+    x = random.randint(1,pos+neg)
+    if x <= pos:
+        return target
+    else:
+        return -target
 
 class MulCell:
     def __init__(self,_num):
         self.num = _num
         self.x = 0
-        self.w = randomfloat(7, 10, 10)
+        self.w = randomfloat(1, 10, 10)
 
     def forward(self,x):
         z = x * self.w
@@ -34,7 +53,7 @@ class AddCell:
     def __init__(self,_num):
         self.num = _num
         self.x = 0
-        self.b = randomfloat(0, 2, 10)
+        self.b = randomfloat(-10, 10, 10)
 
     def forward(self,x):
         z = x + self.b
@@ -52,83 +71,146 @@ class AddCell:
     def setb(self,_b):
         self.b = _b
 
+class Relu:
+    def __init__(self,_num):
+        self.num = _num
+        self.x = 0
+    
+    def forward(self,x):
+        self.x = x
+        if x >= 0:
+            return x
+        else:
+            return 0
+
+    def backward(self,dz):
+        if self.x >= 0:
+            return dz
+        else:
+            return 0
+
+class Neural:
+    def __init__(self,_num):
+        self.num = _num
+        self.cells = [MulCell(0),AddCell(1),Relu(2)]
+
+    def forward(self,x):
+        z = self.cells[0].forward(x)
+        y = self.cells[1].forward(z)
+        s = self.cells[2].forward(y)
+        return s
+
+    def backward(self,dz):
+        ds = self.cells[2].backward(dz)
+        da,db = self.cells[1].backward(ds)
+        dw,dx = self.cells[0].backward(da)
+        return (dw,db)
+
+    def getw(self):
+        return self.cells[0].getw()
+
+    def getb(self):
+        return self.cells[1].getb()
+
+    def setw(self,_w):
+        return self.cells[0].setw(_w)
+
+    def setb(self,_b):
+        return self.cells[1].setb(_b)
+
 class Graph:
     def __init__(self):
-        self.cell_0 = MulCell(0)
-        self.cell_1 = AddCell(1)
-        self.cells = [self.cell_0,self.cell_1]
+        self.neurals = [Neural(0),Neural(1),Neural(2)]
         self.loss = []
         self.epochs = []
     
     def forward(self,x,y):
-        output = x
-        for cell in self.cells:
-            output += cell.forward(output)
-        loss = y - output
+        loss = 0
+        score = 0
+        for neural in self.neurals:
+            score += neural.forward(x)
+        loss = y - score
+        loss /= y
         return loss
 
     def backward(self):
         dz = 1
-        da,db = self.cells[1].backward(dz)
-        dw,dx = self.cells[0].backward(da)
-        #print(f'da:{da},db:{db}')
-        #print(f'dw:{dw},dx:{dx}')
-        return dw,db
+        dwdbs = []
+        for neural in self.neurals:
+            dwdb = neural.backward(dz)
+            dwdbs.append(dwdb)
+        return dwdbs
     
     def sgd(self,x,y):
-        lr = 0.001
-        loss = self.forward(x, y)
-        dw,db = self.backward()
-        _w = loss * dw
-        _b = loss * db
-        w = self.cell_0.getw()
-        b = self.cell_1.getb()
-        w += lr * _w
-        b += lr * _b
-        self.cell_0.setw(w)
-        self.cell_1.setb(b)
-        #print(f'w:{w},b:{b}')
-        print(f'loss:{loss}')
+        lr = 0.0001
+        loss = 0
+        y_total = 0
+        for i in range(len(x)):
+            y_total += y[i]
+            posnegs = []
+            results = self.forward(x[i],y[i])
+            print(x[i],y[i])
+            print(results)
+            posnegs.append(results)
+            loss += abs(results)
+        y_total /= len(x)
+        dwdbs = self.backward()
+        for i in range(3):
+            dw = dwdbs[i][0]
+            db = dwdbs[i][1]
+            _w = posorneg(posnegs, loss) * dw * y_total
+            _b = posorneg(posnegs, loss) * db * y_total
+            w = self.neurals[i].getw()
+            b = self.neurals[i].getb()
+            w += lr * _w
+            b += lr * _b
+            self.neurals[i].setw(w)
+            self.neurals[i].setb(b)
         return loss
 
     def predict(self,test):
         outputs = 0
         true = 0
         for x in test:
-            output = x
-            for cell in self.cells:
-                output += cell.forward(output)
+            output = 0
+            for neural in self.neurals:
+                output += neural.forward(x)
             print(f'input:{x},predict:{output},true:{x * 4}')
             outputs +=output
             true += x*4
         print(f'Accuracy:{(outputs/true)*100}%')
     
     def train(self,_num,x_data,y_data):
-        self.loss = [list(),list(),list()]
+        self.loss = []
         self.epochs = []
         for i in range(_num):
             print(f'epoch: {i}\n')
-            for j in range(3):
-                self.loss[j].append(self.sgd(x_data[j], y_data[j]))
+            loss = self.sgd(x_data, y_data)
+            self.loss.append(loss)
+            print(f'loss: {loss}\n')
             self.epochs.append(i)
             print('\n\n\n')
     
     def visiualize(self):
-        plt.plot(self.epochs,self.loss[0],color = 'green',label = f'data {0}')
-        plt.plot(self.epochs,self.loss[1],color = 'red',label = f'data {1}')
-        plt.plot(self.epochs,self.loss[2],color = 'skyblue',label = f'data {2}')
-        plt.ylim((-1,6))
-        plt.legend()
+        plt.plot(self.epochs,self.loss,color = 'green',label = f'data {0}')
         plt.show()
+    
+    def visualparm(self):
+        for nerual in self.neurals:
+            print(f'w: {nerual.getw()} b: {nerual.getb()}')
+
 
 def main():
-    x_data = [1.0, 2.0, 3.0]
-    y_data = [4.0, 8.0, 12.0]
-    x_test = [3,6,17,5,20]
+    x_data = [2,3,5,8,13,21,34]
+    y_data = [8,12,20,32,52,84,136]
+    x_test = [1,4,6,7,9]
     Graph1 = Graph()
-    Graph1.train(400,x_data,y_data)
+    Graph1.train(120,x_data,y_data)
     Graph1.visiualize()
+    Graph1.predict(x_data)
     Graph1.predict(x_test)
+    #Graph1.visualparm()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
